8311659e1fc85ba7b50188c504802356da57692e
Dan <dan@wadoku.de>
2015-08-12 09:16:51 +0200

    openssh 7.0p1

--- openssh.rb
+++ openssh.rb
@@ -1,16 +1,10 @@
 class Openssh < Formula
   desc "OpenBSD freely-licensed SSH connectivity tools"
   homepage "http://www.openssh.com/"
-  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.9p1.tar.gz"
-  mirror "https://fossies.org/linux/misc/openssh-6.9p1.tar.gz"
-  version "6.9p1"
-  sha256 "6e074df538f357d440be6cf93dc581a21f22d39e236f217fcd8eacbb6c896cfe"
-
-  bottle do
-    sha256 "fa65b9cc13811547bc9b5a44e27b66d756a9c5ae5ef390bb8a028804f177733a" => :yosemite
-    sha256 "c66c391b706da983940b03f3bbae049f31e92557938c9eab27dbc183d47f89ee" => :mavericks
-    sha256 "2a56fb8a854e16280d1da335bfb0c4306659a46b11748f73b6a7e30cb13822e4" => :mountain_lion
-  end
+  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.0p1.tar.gz"
+  mirror "https://www.mirrorservice.org/pub/OpenBSD/OpenSSH/portable/openssh-7.0p1.tar.gz"
+  version "7.0p1"
+  sha256 "fd5932493a19f4c81153d812ee4e042b49bbd3b759ab3d9344abecc2bc1485e5"
 
   # Please don't resubmit the keychain patch option. It will never be accepted.
   # https://github.com/Homebrew/homebrew-dupes/pull/482#issuecomment-118994372
