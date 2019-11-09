723079bbec5a9624ea7a96b21391d059b3b17028
Dan <dan@wadoku.de>
2019-11-09 03:25:49 +0100

    bambootracker 0.3.0

---
+++ Casks/bambootracker.rb
@@ -0,0 +1,10 @@
+cask 'bambootracker' do
+  version '0.3.0'
+  sha256 'c4a7ded7405443b940ab6fcbfaa0b75395b5efde469545ea46b53b6573ef1eb8'
+
+  url "https://github.com/rerrahkr/BambooTracker/releases/download/v0.3.0/BambooTracker-v0.3.0-mac.zip"
+  name 'BambooTracker'
+  homepage 'https://github.com/rerrahkr/BambooTracker'
+
+  app 'BambooTracker.app'
+end
