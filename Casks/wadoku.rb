cec10b588e939e8f9610febbe01f481d424d665b
Dan <dan@wadoku.de>
2021-07-04 16:23:12 +0200

    wadoku 20210704

--- Casks/wadoku.rb
+++ Casks/wadoku.rb
@@ -1,6 +1,6 @@
 cask 'wadoku' do
-  version '20210110'
-  sha256 '1cd1a14d6673ed9cc70f78b4e46b74a9f81a0b58caa7473067b77af9fc1999da'
+  version '20210704'
+  sha256 '9b05de5fdb404e781f2a9fe63829181e68fbcd3104bac49696bbfa0c59bd985b'
 
   url "https://www.wadoku.de/downloads/mac/Wadoku#{version}.dictionary.dmg"
   name 'Wadoku for macOS/iOS'
