7eb5416ce28d89f3c01d74b784d748bded051147
Dan <dan@wadoku.de>
2022-09-10 18:03:47 +0200

    Update bambootracker from 0.3.5 to 0.5.2

--- Casks/bambootracker.rb
+++ Casks/bambootracker.rb
@@ -1,10 +1,10 @@
-cask 'bambootracker' do
-  version '0.3.5'
-  sha256 '61e3fcd3265942840f8e3feded68d7d76cec32bafba8e055deda84ea2d8fb4d0'
+cask "bambootracker" do
+  version "0.5.2"
+  sha256 "fef3db340bef567aee0f91c3145da3a505642aa6c3f928d43e8113c1a4f25fdd"
 
-  url "https://github.com/rerrahkr/BambooTracker/releases/download/v0.3.5/BambooTracker-v0.3.5-macOS.zip"
-  name 'BambooTracker'
-  homepage 'https://github.com/rerrahkr/BambooTracker'
+  url "https://github.com/rerrahkr/BambooTracker/releases/download/v#{version}/BambooTracker-v#{version}-macOS-64bit.zip"
+  name "BambooTracker"
+  homepage "https://github.com/rerrahkr/BambooTracker"
 
-  app 'BambooTracker.app'
+  app "BambooTracker.app"
 end
