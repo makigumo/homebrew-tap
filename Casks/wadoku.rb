1ea722fb0749d730e11ef89c4b787eb2536de18a
Dan <dan@wadoku.de>
2022-09-10 18:54:35 +0200

    wadoku: formatting

--- Casks/wadoku.rb
+++ Casks/wadoku.rb
@@ -1,10 +1,17 @@
-cask 'wadoku' do
-  version '20220703'
-  sha256 'ad53bf10164d90709851cbac4567824539ce5e9a1428a622add10a6d57283f8e'
+cask "wadoku" do
+  version "20220703"
+  sha256 "ad53bf10164d90709851cbac4567824539ce5e9a1428a622add10a6d57283f8e"
 
-  url "https://www.wadoku.de/downloads/mac/Wadoku#{version}.dictionary.dmg"
-  name 'Wadoku for macOS/iOS'
-  homepage 'https://github.com/makigumo/wadoku-mac-dic'
+  url "https://www.wadoku.de/downloads/mac/Wadoku#{version}.dictionary.dmg",
+      verified: "wadoku.de/downloads/mac"
+  name "Wadoku for macOS/iOS"
+  desc "Japanese-German dictionary"
+  homepage "https://github.com/makigumo/wadoku-mac-dic"
 
-  dictionary 'Wadoku.dictionary'
+  livecheck do
+    url "https://www.wadoku.de/downloads/mac/"
+    regex(/Wadoku(\d{8}).dictionary/i)
+  end
+
+  dictionary "Wadoku.dictionary"
 end
