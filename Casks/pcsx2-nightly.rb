ed7c48e252b232d9a8620cc1ee5f6768da61ca1a
Dan <dan@wadoku.de>
2022-09-10 20:01:15 +0200

    pcsx2-qt nightly

---
+++ Casks/pcsx2-nightly.rb
@@ -0,0 +1,18 @@
+cask "pcsx2-nightly" do
+  version "1.7.3300"
+  sha256 "04c61b14cefd30c78ae29eeaf7e88077aa26684233e23d0bc9a2b8633d2242f1"
+
+  url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.gz",
+      verified: "github.com/PCSX2/pcsx2"
+  name "pcsx2-nightly"
+  desc "Playstation 2 Emulator"
+  homepage "https://pcsx2.net/"
+
+  app "PCSX2.app"
+
+  zap trash: [
+    "~/Library/Application Support/PCSX2",
+    "~/Library/Preferences/net.pcsx2.pcsx2.plist",
+    "~/Library/Saved Application State/net.pcsx2.pcsx2.savedState",
+  ]
+end
