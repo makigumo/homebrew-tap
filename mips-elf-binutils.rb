1e382512a378b96b290917ed9fe9fa94ed8eb9da
Dan <dan@wadoku.de>
2022-09-30 21:10:36 +0200

    add mips-elf-binutils

---
+++ mips-elf-binutils.rb
@@ -0,0 +1,40 @@
+class MipsElfBinutils < Formula
+  desc "GNU Binutils for mips-elf cross development"
+  homepage "https://www.gnu.org/software/binutils/"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.39.tar.xz"
+  sha256 "645c25f563b8adc0a81dbd6a41cffbf4d37083a382e02d5d3df4f65c09516d00"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    formula "binutils"
+  end
+
+  depends_on "texinfo" => :build
+
+  def install
+    target = "mips-elf"
+    system "./configure", "--target=#{target}",
+           "--prefix=#{prefix}",
+           "--libdir=#{lib}/#{target}",
+           "--infodir=#{info}/#{target}",
+           "--disable-nls"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test-s.s").write <<~EOS
+       .section .text
+       .globl _start
+       _start:
+       li $t0,1
+      b 0
+       nop
+    EOS
+    system "#{bin}/mips-elf-as", "-o", "test-s.o", "test-s.s"
+    assert_match "file format elf32-bigmips",
+                 shell_output("#{bin}/mips-elf-objdump -a test-s.o")
+    assert_match "f()", shell_output("#{bin}/mips-elf-c++filt _Z1fv")
+  end
+end
