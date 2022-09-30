02b2740fd04f8a21b7dd0c3db086dc4b023775de
Dan <dan@wadoku.de>
2022-09-30 21:40:24 +0200

    rusty-psn 0.3.0

---
+++ rusty-psn.rb
@@ -0,0 +1,17 @@
+class RustyPsn < Formula
+  desc "Get yourself some updates for those ps3 games, will ya?"
+  homepage "https://github.com/RainbowCookie32/rusty-psn"
+  url "https://github.com/RainbowCookie32/rusty-psn/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "ec3e601fdf448bb725b98dc4359e130d1bf3963e3f757c439c5af2ba8ef61313"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "false"
+  end
+end
