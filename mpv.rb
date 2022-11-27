7bd8073c4bc352c4a4bb62882f77309247ddb999
Dan <dan@wadoku.de>
2022-11-27 05:03:57 +0100

    add mpv

---
+++ mpv.rb
@@ -0,0 +1,70 @@
+class Mpv < Formula
+  desc "Media player based on MPlayer and mplayer2"
+  homepage "https://mpv.io"
+  url "https://github.com/mpv-player/mpv/archive/v0.35.0.tar.gz"
+  sha256 "dc411c899a64548250c142bf1fa1aa7528f1b4398a24c86b816093999049ec00"
+  license :cannot_represent
+  head "https://github.com/mpv-player/mpv.git", branch: "master"
+  
+  depends_on "docutils" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python@3.11" => :build
+  depends_on xcode: :build
+  depends_on "ffmpeg"
+  depends_on "jpeg-turbo"
+  depends_on "libarchive"
+  depends_on "libass"
+  depends_on "little-cms2"
+  depends_on "luajit"
+  depends_on "mujs"
+  depends_on "uchardet"
+  depends_on "vapoursynth"
+  depends_on "yt-dlp"
+
+  on_linux do
+    depends_on "alsa-lib"
+  end
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
+
+  patch do
+    url "https://patch-diff.githubusercontent.com/raw/mpv-player/mpv/pull/9885.patch"
+    sha256 "22359f4f42264b4dd6500f0d37783e9887903ab436b1b7cd6cff7e8d3a0475b1"
+  end
+
+  def install
+    # LANG is unset by default on macOS and causes issues when calling getlocale
+    # or getdefaultlocale in docutils. Force the default c/posix locale since
+    # that's good enough for building the manpage.
+    ENV["LC_ALL"] = "C"
+
+    # libarchive is keg-only
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
+
+    args = %W[
+      -Dhtml-build=enabled
+      -Djavascript=enabled
+      -Dlibmpv=true
+      -Dlua=enabled
+      -Dlibarchive=enabled
+      -Duchardet=enabled
+      -Dsysconfdir=#{etc}/mpv
+      -Ddatadir=#{pkgshare}
+      -Dmandir=#{man}
+      -Dlua=luajit
+    ]
+
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+
+    zsh_completion.install "etc/_mpv.zsh"
+  end
+
+  test do
+    system bin/"mpv", "--ao=null", "--vo=null", test_fixtures("test.wav")
+    assert_match "vapoursynth", shell_output(bin/"mpv --vf=help")
+  end
+end
