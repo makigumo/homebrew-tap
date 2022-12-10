ef0e132037d6ce271f95a9ff93365b53400f4eba
makigumo <makigumo@users.noreply.github.com>
2022-12-10 17:53:16 +0100

    Update rpcs3 from 0.0.25-14442 to 0.0.25-14449 (#337)

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,6 +1,6 @@
 cask "rpcs3" do
-  version "0.0.25-14442,809e880bd1bd369f80091d9f9e9cf451cecc0881,809e880b"
-  sha256 "25cffc3554e4c315e758bce742e321087314151e0fe8b21cc0fa4ea494d31729"
+  version "0.0.25-14449,e2c1e656b9a9d4ebbfcc96e91a11b9558aade7c1,e2c1e656"
+  sha256 "cac6c1b00f46fd393a00099650a9925f6862ec79a92ddf5f844f20ee925adefe"
 
   url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.third}_macos.dmg",
       verified: "github.com/RPCS3/rpcs3-binaries-mac"
@@ -9,8 +9,8 @@ cask "rpcs3" do
   homepage "https://rpcs3.net/"
 
   livecheck do
-    url "https://rpcs3.net/download"
-    regex(%r{href=.*/rpcs3-binaries-mac/releases/download/build-(.+)/rpcs3-v(\d+\.\d+\.\d+-\d+)-(.+)_macos.dmg}i)
+    url "https://api.github.com/repos/RPCS3/rpcs3-binaries-mac/releases/latest"
+    regex(%r{.*/releases/download/build-(.+)/rpcs3-v(\d+\.\d+\.\d+-\d+)-(.+)_macos.dmg}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map do |match|
         "#{match[1]},#{match[0]},#{match[2]}"
