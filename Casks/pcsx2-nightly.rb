53c7b61c0d7ccd94c71704568c58515c4783aea4
makigumo <makigumo@users.noreply.github.com>
2023-02-03 16:16:20 +0100

    Update pcsx2-nightly from 1.7.4000 to 1.7.4001 (#539)

--- Casks/pcsx2-nightly.rb
+++ Casks/pcsx2-nightly.rb
@@ -1,6 +1,6 @@
 cask "pcsx2-nightly" do
-  version "1.7.4000"
-  sha256 "94c50ecec3e5310c5272fe2ffeb2e4c9e8ff5fa6ee649606ecf876e3c710bcdf"
+  version "1.7.4001"
+  sha256 "701e29ebf18e2fa518134b2ba898b018e15de5fd1123f1a86756a0cb1b8a8915"
 
   url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.gz",
       verified: "github.com/PCSX2/pcsx2"
