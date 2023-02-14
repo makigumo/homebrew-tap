4bc98dbbe10581abf8ba8d7210a8cf2b46466211
makigumo <makigumo@users.noreply.github.com>
2023-02-14 09:06:52 +0100

    Update uefitool-ne from A63 to A64 (#593)

--- Casks/uefitool-ne.rb
+++ Casks/uefitool-ne.rb
@@ -1,8 +1,8 @@
 cask "uefitool-ne" do
-  version "A63"
-  sha256 "194b62b455a5e2da4256c9cc420bb409c6b296d4306bc2179954702267e88e76"
+  version "A64"
+  sha256 "3a3346215478d5feb79eeae155119bccdd9f807029172e012dbf9f1445e5d7dc"
 
-  url "https://github.com/LongSoft/UEFITool/releases/download/#{version}/UEFITool_NE_#{version}_mac.zip"
+  url "https://github.com/LongSoft/UEFITool/releases/download/#{version}/UEFITool_NE_#{version}_universal_mac.zip"
   name "UEFITool NE"
   desc "UEFI firmware image viewer and editor"
   homepage "https://github.com/LongSoft/UEFITool"
