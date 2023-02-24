c2073de84872506881f3f8f3772ca2d1ad42c738
Dan <dan@wadoku.de>
2023-02-24 05:03:50 +0100

    p7zip 17.05

---
+++ p7zip.rb
@@ -0,0 +1,28 @@
+class P7zip < Formula
+  desc "7-Zip (high compression file archiver) implementation"
+  homepage "https://github.com/p7zip-project/p7zip"
+  url "https://github.com/p7zip-project/p7zip/archive/v17.05.tar.gz"
+  sha256 "9473e324de6a87d89cb7ff65b0fec4ae3f147f03ffc138189c336a4650d74804"
+  license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
+
+  def install
+    if OS.mac?
+      mv "makefile.macosx_llvm_64bits", "makefile.machine"
+    else
+      mv "makefile.linux_any_cpu", "makefile.machine"
+    end
+    system "make", "all3",
+                   "CC=#{ENV.cc} $(ALLFLAGS)",
+                   "CXX=#{ENV.cxx} $(ALLFLAGS)"
+    system "make", "DEST_HOME=#{prefix}",
+                   "DEST_MAN=#{man}",
+                   "install"
+  end
+
+  test do
+    (testpath/"foo.txt").write("hello world!\n")
+    system bin/"7z", "a", "-t7z", "foo.7z", "foo.txt"
+    system bin/"7z", "e", "foo.7z", "-oout"
+    assert_equal "hello world!\n", File.read(testpath/"out/foo.txt")
+  end
+end
