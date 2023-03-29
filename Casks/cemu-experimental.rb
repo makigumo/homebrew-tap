8d6af95ce5126f5fb3f1b81b980a4d3e4adf036e
Dan <dan@wadoku.de>
2023-03-29 19:33:30 +0200

    Update cemu-experimental from 2.0-30 to 2.0-31

--- Casks/cemu-experimental.rb
+++ Casks/cemu-experimental.rb
@@ -1,6 +1,6 @@
 cask "cemu-experimental" do
-  version "2.0-30"
-  sha256 "4267183dbf080f7d20a484d4422a97607dffc981a70bcf18bc4895746c35bcda"
+  version "2.0-31"
+  sha256 "ad534452fda3887a3e335ba141f045908acb2ba20a41cd8877573e4176130584"
 
   url "https://github.com/cemu-project/Cemu/releases/download/v#{version}/cemu-#{version}-macos-12-x64.dmg",
       verified: "github.com/cemu-project/Cemu"
