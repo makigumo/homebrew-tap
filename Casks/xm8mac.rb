f069daa0966d332196127878dd4dc0696a8c0d50
Dan <dan@wadoku.de>
2023-04-15 19:36:39 +0200

    xm8mac 1.7.2

--- Casks/xm8mac.rb
+++ Casks/xm8mac.rb
@@ -1,9 +1,9 @@
 cask "xm8mac" do
   arch arm: "AppleSilicon", intel: "x86_64"
 
-  version "1.7.1"
-  sha256 arm:   "c611008a7ebccc676b9d5cfeaaebc58e2598511710cffbc8e4b7039e303bd623",
-         intel: "bbd65583f76141122b73969ae51143daec442522470967ba542ad105c6463f5a"
+  version "1.7.2"
+  sha256 arm:   "a6092e93d01c15e14d8cc146b5a2e57b0545b75c50dba1a4f8a6d195b2da9ba5",
+         intel: "91a07be15f1c724880d6214a7bc96563232c64c917c34ec3f2ae377dac2b2f40"
 
   url "https://github.com/bubio/xm8mac/releases/download/#{version}/XM8_macOS_#{arch}.zip"
   name "xm8mac"
