327324d0698f4909292180f5e0fdec978130bcda
makigumo <makigumo@users.noreply.github.com>
2023-05-06 16:26:17 +0200

    xm8mac 1.7.3 (#875)

--- Casks/xm8mac.rb
+++ Casks/xm8mac.rb
@@ -1,11 +1,11 @@
 cask "xm8mac" do
   arch arm: "AppleSilicon", intel: "x86_64"
 
-  version "1.7.2"
-  sha256 arm:   "a6092e93d01c15e14d8cc146b5a2e57b0545b75c50dba1a4f8a6d195b2da9ba5",
-         intel: "91a07be15f1c724880d6214a7bc96563232c64c917c34ec3f2ae377dac2b2f40"
+  version "1.7.3"
+  sha256 arm:   "6f69e52fce69123219146158b445268148061cb52fc6b0a2f73d58129f4e4ec1",
+         intel: "a84328a24302953d70f9bf39bb2c292759bb779b4dc75db96e6288b1e2a14b9b"
 
-  url "https://github.com/bubio/xm8mac/releases/download/#{version}/XM8_macOS_#{arch}.zip"
+  url "https://github.com/bubio/xm8mac/releases/download/#{version}/XM8_macOS_#{arch}.dmg"
   name "xm8mac"
   desc "PC-8801 emulator"
   homepage "https://github.com/bubio/xm8mac"
