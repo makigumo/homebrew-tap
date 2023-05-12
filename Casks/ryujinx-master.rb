cea748ba4d279dea11798693d686ee10738f7e03
Dan <dan@wadoku.de>
2023-05-13 01:46:35 +0200

    ryujinx-master 1.1.795

--- Casks/ryujinx-master.rb
+++ Casks/ryujinx-master.rb
@@ -1,8 +1,8 @@
 cask "ryujinx-master" do
-  version "1.1.792"
-  sha256 "fa73f19bb3255e1f40f651181a6251e33c43d28772556df3a12ed6fb54177ff4"
+  version "1.1.795"
+  sha256 "d9d09cb73bb9aa7746c4ff361847907a2c88472a14ba996537cbae0edbbb2bec"
 
-  url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/Ryujinx-#{version}-macos_universal.app.tar.gz",
+  url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/test-ava-ryujinx-#{version}-macos_universal.app.tar.gz",
       verified: "github.com/Ryujinx/release-channel-master/"
   name "Ryujinx"
   desc "Experimental Nintendo Switch Emulator written in C#"
