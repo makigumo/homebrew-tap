514e3a04b4ad8b7c1687f9bf27874b55b5e45e5c
Dan <dan@wadoku.de>
2023-06-16 11:57:11 +0200

    cemu-experimental 2.0-40

--- Casks/cemu-experimental.rb
+++ Casks/cemu-experimental.rb
@@ -1,6 +1,6 @@
 cask "cemu-experimental" do
-  version "2.0-39"
-  sha256 "353bfa8508b6362063d9d6fe898b558bb739efd20ae7f4a50853eb6ec2fdf89a"
+  version "2.0-40"
+  sha256 "00bc4779ce04614108b974839d3c096a9bf02d391fab746661983a63e26dacb4"
 
   url "https://github.com/cemu-project/Cemu/releases/download/v#{version}/cemu-#{version}-macos-12-x64.dmg",
       verified: "github.com/cemu-project/Cemu/"
