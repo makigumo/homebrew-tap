893029f3ab9d97adf0e5556cf199fbeb2043ee34
Dan <dan@wadoku.de>
2023-07-02 16:12:41 +0200

    wadoku 20230702

--- Casks/wadoku.rb
+++ Casks/wadoku.rb
@@ -1,9 +1,9 @@
 cask "wadoku" do
-  version "20230108"
-  sha256 "8cbbb0f89f82b82f3792ac4fd2d883b0214272c940e3cfbc59ff305c0d10fd26"
+  version "20230702"
+  sha256 "d2efcc5f6005ac4b15fd9b96045053a1cb03fa3735d6f6d36abb34db996901b8"
 
   url "https://www.wadoku.de/downloads/mac/Wadoku#{version}.dictionary.dmg",
-      verified: "wadoku.de/downloads/mac"
+      verified: "wadoku.de/downloads/mac/"
   name "Wadoku for macOS/iOS"
   desc "Japanese-German dictionary"
   homepage "https://github.com/makigumo/wadoku-mac-dic"
