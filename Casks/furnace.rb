f2892dc15846d7db9b0618cbd6d740ab6cb771ee
makigumo <makigumo@users.noreply.github.com>
2023-08-28 14:22:31 +0200

    furnace 0.6pre9 (#1404)

--- Casks/furnace.rb
+++ Casks/furnace.rb
@@ -1,9 +1,9 @@
 cask "furnace" do
   arch arm: "arm64", intel: "Intel"
 
-  version "0.6pre7"
-  sha256 arm:   "8863c7f7becea62c2a65e4121e2cfe4d843d23e50e483eb68516df3301c9bbbc",
-         intel: "74d82a21ce32bed02864493f45626e0aba3a174c50cf27efe69893a1ba69da35"
+  version "0.6pre9"
+  sha256 arm:   "5bf21eda55e4c96595b7a93f8936df0f06d15e284a4435fccad8f87e1298580a",
+         intel: "1945dd99f9a3416e7cd6ce1d72d9f8b549f3e5801a38a534e76522f6dd696a3b"
 
   url "https://github.com/tildearrow/furnace/releases/download/v#{version}/furnace-#{version}-mac-#{arch}.dmg"
   name "furnace"
@@ -12,12 +12,12 @@ cask "furnace" do
 
   livecheck do
     url :url
-    regex(%r{/releases/tag/v(.+?)"}i)
-    strategy :github_latest do |page, regex|
-      match = page.match(regex)
+    regex(/v?(\d+(?:\.\d+)+(?:pre\d+)?)$/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
       next if match.blank?
 
-      (match[1]).to_s
+      match[1]
     end
   end
 
