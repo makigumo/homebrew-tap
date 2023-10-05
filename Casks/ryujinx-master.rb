697d5d116e6e3f1d0c1eeacff7858ffd72d8a254
Dan <dan@wadoku.de>
2023-10-05 15:09:20 +0200

    ryujinx-master 1.1.1044

--- Casks/ryujinx-master.rb
+++ Casks/ryujinx-master.rb
@@ -1,6 +1,6 @@
 cask "ryujinx-master" do
-  version "1.1.1042"
-  sha256 "7f0679d9284cf03e5bc1bc19158613b56ab5510f15af650f5abf045aee258a70"
+  version "1.1.1044"
+  sha256 "5286e836f949a08f9b39366f061d42c289b7f4dea4420e89771c1a0c47b6ec35"
 
   url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/test-ava-ryujinx-#{version}-macos_universal.app.tar.gz",
       verified: "github.com/Ryujinx/release-channel-master/"
