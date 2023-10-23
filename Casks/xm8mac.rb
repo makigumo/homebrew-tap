69bc5cb422f0793fb0b143490d95124d9cff6b01
makigumo <makigumo@users.noreply.github.com>
2023-10-23 16:43:03 +0200

    xm8mac 1.7.4 (#1585)

--- Casks/xm8mac.rb
+++ Casks/xm8mac.rb
@@ -1,9 +1,9 @@
 cask "xm8mac" do
   arch arm: "AppleSilicon", intel: "x86_64"
 
-  version "1.7.3"
-  sha256 arm:   "6f69e52fce69123219146158b445268148061cb52fc6b0a2f73d58129f4e4ec1",
-         intel: "a84328a24302953d70f9bf39bb2c292759bb779b4dc75db96e6288b1e2a14b9b"
+  version "1.7.4"
+  sha256 arm:   "a38139971d194c90fb65d19d5a6e18f380b98f22677127c54d7a74512d270ea2",
+         intel: "53c08bcf4571185cacd38a726d542ea815d8860a6711b586da845cd0c207e301"
 
   url "https://github.com/bubio/xm8mac/releases/download/#{version}/XM8_macOS_#{arch}.dmg"
   name "xm8mac"
