54db9260955a6fdd9dc7e2a23fde66d1a8f7646a
Dan <dan@wadoku.de>
2023-11-24 18:36:58 +0100

    hexfiend 2.17.0

--- Casks/hexfiend.rb
+++ Casks/hexfiend.rb
@@ -1,8 +1,8 @@
 cask "hexfiend" do
-  version "2.17.0b3"
-  sha256 "0284db3763b5a4bd0a29be7bee0dfd7f6f855d4936a1310830f2fe46c9e86d4b"
+  version "2.17.0"
+  sha256 "e3c7b3591dd50480907b16dae20ca57576f3e2f734fb8c1fc422be0f8c83f962"
 
-  url "https://github.com/HexFiend/HexFiend/releases/download/#{version}/Hex_Fiend_#{version.major}.#{version.minor}.dmg",
+  url "https://github.com/HexFiend/HexFiend/releases/download/v#{version}/Hex_Fiend_#{version.major}.#{version.minor}.dmg",
       verified: "github.com/HexFiend/HexFiend/"
   name "HexFiend"
   desc "Fast and clever hex editor"
