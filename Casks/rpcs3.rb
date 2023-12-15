eff1264dc54db357eb3a552cee9b857c304cdd65
makigumo <makigumo@users.noreply.github.com>
2023-12-15 19:19:01 +0100

    rpcs3 0.0.29-15809 (#1703)

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,20 +1,25 @@
 cask "rpcs3" do
-  version "0.0.29-15786,fbeaa1c487437fe90a565803f4d45738952c1f08,fbeaa1c4"
-  sha256 "056362c5e026dacbe1b3d126c17ffdbe9609977fbceec86ef8189d52a5839b1f"
+  version "0.0.29-15809,f8e0c9e27ab81d586dfac0e228074d4d5e848934,f8e0c9e2"
+  sha256 "c0705fb96471ca29a42a8ecd3f468880bb53f2656995922a845e61c768f4c76f"
 
-  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.third}_macos.dmg",
+  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.third}_macos.7z",
       verified: "github.com/RPCS3/rpcs3-binaries-mac/"
   name "RPCS3"
   desc "PlayStation 3 emulator and debugger"
   homepage "https://rpcs3.net/"
 
   livecheck do
-    url "https://api.github.com/repos/RPCS3/rpcs3-binaries-mac/releases/latest"
-    regex(%r{.*/releases/download/build-(.+)/rpcs3-v(\d+\.\d+\.\d+-\d+)-(.+)_macos.dmg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map do |match|
-        "#{match[1]},#{match[0]},#{match[2]}"
-      end
+    url :url
+    regex(%r{^.+build-(.+)/rpcs3-v(\d+\.\d+\.\d+-\d+)-(.+)_.*$}i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        release["assets"]&.map do |asset|
+          match = asset["browser_download_url"]&.match(regex)
+          next if match.blank?
+
+          "#{match[2]},#{match[1]},#{match[3]}"
+        end
+      end.flatten
     end
   end
 
