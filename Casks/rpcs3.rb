a45fe7e3edd343205763c33b3881a71a684c3680
Dan <dan@wadoku.de>
2023-12-18 10:40:12 +0100

    rpcs3 0.0.29-15812

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,6 +1,6 @@
 cask "rpcs3" do
-  version "0.0.29-15809,f8e0c9e27ab81d586dfac0e228074d4d5e848934,f8e0c9e2"
-  sha256 "c0705fb96471ca29a42a8ecd3f468880bb53f2656995922a845e61c768f4c76f"
+  version "0.0.29-15812,19d1e4dcc9a3db12d12917194a56ec914f60d47d,19d1e4dc"
+  sha256 "7660bcc9bf381151e4a08b1e4e51b04036862485737a030039e7ba7719a02c3c"
 
   url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.third}_macos.7z",
       verified: "github.com/RPCS3/rpcs3-binaries-mac/"
@@ -9,17 +9,12 @@ cask "rpcs3" do
   homepage "https://rpcs3.net/"
 
   livecheck do
-    url :url
-    regex(%r{^.+build-(.+)/rpcs3-v(\d+\.\d+\.\d+-\d+)-(.+)_.*$}i)
-    strategy :github_releases do |json, regex|
-      json.map do |release|
-        release["assets"]&.map do |asset|
-          match = asset["browser_download_url"]&.match(regex)
-          next if match.blank?
-
-          "#{match[2]},#{match[1]},#{match[3]}"
-        end
-      end.flatten
+    url "https://api.github.com/repos/RPCS3/rpcs3-binaries-mac/releases/latest"
+    regex(%r{.*/releases/download/build-(.+)/rpcs3-v(\d+\.\d+\.\d+-\d+)-(.+)_macos.7z}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        "#{match[1]},#{match[0]},#{match[2]}"
+      end
     end
   end
 
