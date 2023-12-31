7481ffbf4245e230eff3a47b962f25c47084fe2d
makigumo <makigumo@users.noreply.github.com>
2023-12-31 10:43:21 +0100

    rpcs3 0.0.29-15877 (#1749)

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,6 +1,6 @@
 cask "rpcs3" do
-  version "0.0.29-15869,d4796c46b1195bb9e473f96f94d6f8991f036ac7,d4796c46"
-  sha256 "dd9bec220dc9fbbe11e862f60f346570e0a4075952a8323e22f7ef54103d4056"
+  version "0.0.29-15877,dfb163546bdc8206d4757e848588b7e53424327b,dfb16354"
+  sha256 "0c63d6a499efc882a6f87e0d34d6c3056440a0b2ff0d02589815b0565dfd0831"
 
   url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.third}_macos.7z",
       verified: "github.com/RPCS3/rpcs3-binaries-mac/"
