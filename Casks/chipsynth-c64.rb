8c57aca48e107da8a53a146217e57c3e0b38d414
Dan <dan@wadoku.de>
2024-01-16 11:04:20 +0100

    chipsynth-c64 1.005

--- Casks/chipsynth-c64.rb
+++ Casks/chipsynth-c64.rb
@@ -1,10 +1,11 @@
 cask "chipsynth-c64" do
-  version "1.004"
-  sha256 "87bde32e6b9e1498f14bd5929557223646424dced676c5d4939bccfc3c872035"
+  version "1.005"
+  sha256 "a7fcee6b380f035dcd0648bd9f61f8f5de7cdaedf77585718a51e1064f5b7d62"
 
-  url "https://s3.amazonaws.com/chipsynth/MAC_chipsynth_C64_v#{version}.dmg"
+  url "https://s3.amazonaws.com/chipsynth/MAC_chipsynth_C64_v#{version}.dmg",
+      verified: "s3.amazonaws.com/chipsynth"
   name "chipsynth-c64"
-  desc "sid synthesizer"
+  desc "Sid synthesizer"
   homepage "https://plogue.com/products/chipsynth-c64.html"
 
   livecheck do
@@ -15,7 +16,7 @@ cask "chipsynth-c64" do
 
   pkg "MAC_chipsynth_C64_v#{version}.pkg"
 
-  uninstall pkgutil:
+  uninstall pkgutil: []
 
   zap trash: [
     "/Library/Preferences/com.Plogue Art et Technologie, Inc.chipsynth C64.plist",
