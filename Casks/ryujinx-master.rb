48b994ff1869dca89bda3e834cc56bf03613500c
makigumo <makigumo@users.noreply.github.com>
2024-06-26 23:02:59 +0200

    ryujinx-master 1.1.1340 (#2331)

--- Casks/ryujinx-master.rb
+++ Casks/ryujinx-master.rb
@@ -1,6 +1,6 @@
 cask "ryujinx-master" do
-  version "1.1.1338"
-  sha256 "908bd5b4368ee71f3e2eae15ca3b23b04c6f574cd0ee4808b485c3ee85916045"
+  version "1.1.1340"
+  sha256 "a0c6c7b906121508d922fb44dd4bb74bfb3c9cb0d68b9dd69f384e7c86ba0cde"
 
   url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/test-ava-ryujinx-#{version}-macos_universal.app.tar.gz",
       verified: "github.com/Ryujinx/release-channel-master/"
