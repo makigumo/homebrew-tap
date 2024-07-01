e6afe9783132fb3f706f06f3c066e77b14573162
Dan <dan@wadoku.de>
2024-07-01 09:26:43 +0200

    furnace 0.6.5

--- Casks/furnace.rb
+++ Casks/furnace.rb
@@ -1,9 +1,9 @@
 cask "furnace" do
   arch arm: "arm64", intel: "Intel"
 
-  version "0.6.3"
-  sha256 arm:   "284d9d4efc4e7f0d1c41cc4f1b9c056767e2064a8961e1104bc2f1bd83fb8b7f",
-         intel: "9e2f38bd6be06f8f2ff4da8113cbe325368d6fe9031289ed1b1b47004ef3bcf1"
+  version "0.6.5"
+  sha256 arm:   "1bcc8c6c655ec2a3e3f770588cb28a955208227c03f33ae4b22decf19fd7efbb",
+         intel: "8391e2118f61a4b958dafbccb2867f36680302a8770cfad6466720f291e794c0"
 
   url "https://github.com/tildearrow/furnace/releases/download/v#{version}/furnace-#{version}-mac-#{arch}.dmg"
   name "furnace"
