602bd1414984c17219ad81997b6cede1e2079297
makigumo <makigumo@users.noreply.github.com>
2024-09-30 10:46:52 +0200

    rusty-psn 0.5.1 (#2624)

--- rusty-psn.rb
+++ rusty-psn.rb
@@ -1,13 +1,11 @@
 class RustyPsn < Formula
   desc "Get yourself some updates for those ps3 games, will ya?"
   homepage "https://github.com/RainbowCookie32/rusty-psn"
-  url "https://github.com/RainbowCookie32/rusty-psn/archive/refs/tags/v0.4.6.tar.gz"
-  sha256 "12ff28add595dc038d9520892738ea89aa637789e3d0a55a2ede8b236c8918f6"
+  url "https://github.com/RainbowCookie32/rusty-psn/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "f6791a0d13ff52832c6cc26d7deba82bf4527bde56a4d62d971afac63307da69"
   license "MIT"
 
   depends_on "rust" => :build
-
-  patch :DATA
 
   def install
     # update version
@@ -20,21 +18,3 @@ class RustyPsn < Formula
     system "false"
   end
 end
-__END__
-diff --git a/src/utils.rs b/src/utils.rs
-index b66214f..6769e00 100644
---- a/src/utils.rs
-+++ b/src/utils.rs
-@@ -10,10 +10,10 @@ use tokio::io::{AsyncReadExt, AsyncSeekExt, SeekFrom};
- 
- use crate::psn::DownloadError;
- 
--#[cfg(target_os = "windows")]
-+#[cfg(target_family = "windows")]
- const INVALID_CHARS: [char; 9] = ['<', '>', ':', '"', '/', '\\', '|', '?', '*'];
- 
--#[cfg(target_os = "linux")]
-+#[cfg(target_family = "unix")]
- const INVALID_CHARS: [char; 1] = ['/'];
- 
- fn sanitize_title(title: &str) -> String {
