cdb1ed174686788f51744fa44d6239763d3a4fb6
Dan <dan@wadoku.de>
2024-09-30 10:51:11 +0200

    bambootracker 0.6.4

--- Casks/bambootracker.rb
+++ Casks/bambootracker.rb
@@ -1,11 +1,11 @@
 cask "bambootracker" do
-  version "0.6.3"
-  sha256 "fec4cba21aa525ebd23a9901a7e6aae36cb1eab6f4985312fc5a6c950c5d81b4"
+  version "0.6.4"
+  sha256 "60fc024f1ae75b1ccc8942cefb50249970b56cedf2e2a4fc13f786894f31650b"
 
-  url "https://github.com/rerrahkr/BambooTracker/releases/download/v#{version}/BambooTracker-v#{version}-macOS-64bit.zip"
+  url "https://github.com/BambooTracker/BambooTracker/releases/download/v#{version}/BambooTracker-v#{version}-macos-64bit.tar.gz"
   name "BambooTracker"
   desc "YM2608 music tracker"
-  homepage "https://github.com/rerrahkr/BambooTracker"
+  homepage "https://github.com/BambooTracker/BambooTracker"
 
   app "BambooTracker.app"
 end
