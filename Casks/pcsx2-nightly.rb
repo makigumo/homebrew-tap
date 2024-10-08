73f66d4b163573c9989bfc27a199f641e8b87b86
Dan <dan@wadoku.de>
2024-10-09 01:46:48 +0200

    pcsx2-nightly 2.1.193

--- Casks/pcsx2-nightly.rb
+++ Casks/pcsx2-nightly.rb
@@ -1,6 +1,6 @@
 cask "pcsx2-nightly" do
-  version "2.1.189"
-  sha256 "ae92e7c260fe6839b2cca61a2784b3716dbfd701357ef2300f02f4a53e014020"
+  version "2.1.193"
+  sha256 "d9af56243c95f16692ea4832f49dfe93b8f939993e1774024ac1d4fe643f86b4"
 
   url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.xz",
       verified: "github.com/PCSX2/pcsx2/"
