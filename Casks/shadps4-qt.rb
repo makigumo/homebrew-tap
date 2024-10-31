a17df289cbf7c68de7ade8dce9af013d6f0573cc
Dan <dan@wadoku.de>
2024-10-31 20:21:24 +0100

    shadps4-qt 0.4.0

--- Casks/shadps4-qt.rb
+++ Casks/shadps4-qt.rb
@@ -1,6 +1,6 @@
 cask "shadps4-qt" do
-  version "0.3.0"
-  sha256 "584304657de7880462680b5729ecd3935d9b3a49f0e4852085aad88acc5e454c"
+  version "0.4.0"
+  sha256 "7ccc7794664084f9818ad0884a7d8757e7b1ea94ac373661f498f8b0e75b7957"
 
   url "https://github.com/shadps4-emu/shadPS4/releases/download/v.#{version}/shadps4-macos-qt-#{version}.zip",
       verified: "github.com/shadps4-emu/shadPS4/"
