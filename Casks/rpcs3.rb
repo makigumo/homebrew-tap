a271b12d5276f9648cf69bb7679b6e8a35f2c3e7
Dan <dan@wadoku.de>
2024-11-22 18:48:59 +0100

    rpcs3 0.0.34-17142

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,6 +1,6 @@
 cask "rpcs3" do
-  version "0.0.34-17138,0111fd0d0e0698a80c8fc29b8b52640dd76ae4b2,0111fd0d"
-  sha256 "749e0b6f36227ac1b04f9ea89989853785ff3dd8063f0e0a0bfcf19b0602ed94"
+  version "0.0.34-17142,0e8f1a9bf9f7483dde93f0a05fd682b4bb68196f,0e8f1a9b"
+  sha256 "373bb2c557c6573c62014e067fcc8a0a03249c6dbe1cf33c35d041a36a23c956"
 
   url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.third}_macos.7z",
       verified: "github.com/RPCS3/rpcs3-binaries-mac/"
