a465c89435bb68b19266958c505c00aae147254f
Dan <dan@wadoku.de>
2024-12-21 08:07:44 +0100

    jmdict 20241221.12442487751

--- Casks/jmdict.rb
+++ Casks/jmdict.rb
@@ -1,10 +1,11 @@
-cask 'jmdict' do
-  version 'latest'
-  sha256 :no_check
+cask "jmdict" do
+  version "20241221.12442487751"
+  sha256 "70cb35ece3fd99e2b8f0920bbff3f4e1859ed8564f8d8ba6883daa8b934532a0"
 
-  url "https://github.com/makigumo/jmdict-mac-dic/releases/latest/download/JMDict.dmg"
-  name 'JMDict for Mac'
-  homepage 'https://github.com/makigumo/jmdict-mac-dic'
+  url "https://github.com/makigumo/jmdict-mac-dic/releases/download/#{version}/JMDict.dmg"
+  name "JMDict for Mac"
+  desc "Japanese-English dictionary"
+  homepage "https://github.com/makigumo/jmdict-mac-dic"
 
-  dictionary 'JMDict.dictionary'
+  dictionary "JMDict.dictionary"
 end
