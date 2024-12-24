36fba7f192e08f27a3456b86417821f9617717b7
Dan <dan@wadoku.de>
2024-12-24 13:24:51 +0100

    ryujinx@canary 1.2.125

---
+++ Casks/ryujinx@canary.rb
@@ -0,0 +1,17 @@
+cask "ryujinx@canary" do
+  version "1.2.125"
+  sha256 "9e2a2b34d8c0d30d14c28147c9092138f0be9d8375bcd1319528eb48014f5ec8"
+
+  url "https://github.com/GreemDev/Ryujinx-Canary/releases/download/#{version}/ryujinx-canary-#{version}-macos_universal.app.tar.gz"
+  name "Ryujinx"
+  desc "Experimental Nintendo Switch Emulator written in C#"
+  homepage "https://github.com/GreemDev/Ryujinx"
+
+  app "Ryujinx.app", target: "Ryujinx Canary.app"
+
+  zap trash: [
+    "~/Library/Preferences/org.ryujinx.Ryujinx.plist",
+    "~/Library/Preferences/Ryujinx.plist",
+    "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
+  ]
+end
