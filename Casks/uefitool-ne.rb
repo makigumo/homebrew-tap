81a370ce3019e3d78ce77eaac38dcd9c0f2b9618
makigumo <makigumo@users.noreply.github.com>
2025-02-21 21:52:21 +0100

    uefitool-ne 69 (#2983)

--- Casks/uefitool-ne.rb
+++ Casks/uefitool-ne.rb
@@ -1,6 +1,6 @@
 cask "uefitool-ne" do
-  version "68"
-  sha256 "7a43a6b0272919119ef89bcb3bd6eb3113755040574cad0d7c0bec9918a50439"
+  version "69"
+  sha256 "2ffc26b973e1fb68f74d5bfedebc594534f5eaf3ccf2b0087c99c7f3f5b5bb79"
 
   url "https://github.com/LongSoft/UEFITool/releases/download/A#{version}/UEFITool_NE_A#{version}_universal_mac.zip"
   name "UEFITool NE"
