e2888418aa1e91c4614064e15e9bfea76a89b3b8
Dan <dan@wadoku.de>
2025-03-23 07:09:16 +0100

    shadps4-qt 0.7.0

--- Casks/shadps4-qt.rb
+++ Casks/shadps4-qt.rb
@@ -1,12 +1,14 @@
 cask "shadps4-qt" do
-  version "0.6.0"
-  sha256 "c6cbf95474ac51e41bf67a3579fcc5d3397e232f50bd9e61eac9e9a6f5f560a8"
+  version "0.7.0"
+  sha256 "413bbeca111c4d920f9536f662e5b06bf4c911c2a4e696033e81864c8acb64ea"
 
   url "https://github.com/shadps4-emu/shadPS4/releases/download/v.#{version}/shadps4-macos-qt-#{version}.zip",
       verified: "github.com/shadps4-emu/shadPS4/"
   name "shadps4-qt"
   desc "PS4 emulator"
   homepage "https://shadps4.net/"
+
+  depends_on macos: :sonoma
 
   app "shadps4.app"
 
