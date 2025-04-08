536d9ab5c409371cead9d1491cacd71f248d425e
makigumo <makigumo@users.noreply.github.com>
2025-04-08 08:11:25 +0200

    ryujinx@canary 1.3.30 (#3140)

--- Casks/ryujinx@canary.rb
+++ Casks/ryujinx@canary.rb
@@ -1,6 +1,6 @@
 cask "ryujinx@canary" do
-  version "1.3.29"
-  sha256 "cf8989dd7b71769759a5370c2daea36ca33daca64bcb63572555fe8e0e8aba2c"
+  version "1.3.30"
+  sha256 "54ba0c77a1878ff452866e231b7292e45608541d243a4461e5334ba8a0beb992"
 
   url "https://github.com/GreemDev/Ryujinx-Canary/releases/download/#{version}/ryujinx-canary-#{version}-macos_universal.app.tar.gz",
       verified: "github.com/GreemDev/Ryujinx-Canary/"
