c83405259f2cb08b661b97ccb41c8fa24380da4f
makigumo <makigumo@users.noreply.github.com>
2025-05-13 15:13:55 +0200

    pcsx2@nightly 2.3.343 (#3234)

--- Casks/pcsx2@nightly.rb
+++ Casks/pcsx2@nightly.rb
@@ -1,6 +1,6 @@
 cask "pcsx2@nightly" do
-  version "2.3.337"
-  sha256 "fd340fa5cb732dfa21d705e7fc2621de83e15be07b05185f504ad317ea7cacb0"
+  version "2.3.343"
+  sha256 "7029f5713efc884808e089bfb247ffb3d2dea70e8b35383d1b266d72ce2d738d"
 
   url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.xz",
       verified: "github.com/PCSX2/pcsx2/"
