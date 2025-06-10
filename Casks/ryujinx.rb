3314be65cb28cb128ecb858d49a1fb8cb5cd003b
Dan <dan@wadoku.de>
2025-06-10 22:29:51 +0200

    ryujinx 1.3.2

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -1,17 +1,26 @@
 cask "ryujinx" do
-  version "1.2.81"
-  sha256 "ad444ebe3cd03ee1cda30723f7f8d16e4319f6c2374e9d2f9ad8f19adf04a724"
+  version "1.3.2"
+  sha256 "c157907318348999ce4fb8c8fe8ddd511f0ddea643f22b1529527390180cb700"
 
-  url "https://github.com/GreemDev/Ryujinx/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
+  url "https://git.ryujinx.app/api/v4/projects/1/packages/generic/Ryubing/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
   desc "Experimental Nintendo Switch Emulator written in C#"
-  homepage "https://github.com/GreemDev/Ryujinx"
+  homepage "https://ryujinx.app/"
+
+  livecheck do
+    url "https://git.ryujinx.app/api/v4/projects/1/releases"
+    strategy :json do |json, _regex|
+      json.map { |item| item["tag_name"] }
+    end
+  end
+
+  depends_on macos: ">= :monterey"
 
   app "Ryujinx.app", target: "Ryujinx GreemDev.app"
 
   zap trash: [
-    "~/Library/Preferences/Ryujinx.plist",
     "~/Library/Preferences/org.ryujinx.Ryujinx.plist",
+    "~/Library/Preferences/Ryujinx.plist",
     "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
   ]
 end
