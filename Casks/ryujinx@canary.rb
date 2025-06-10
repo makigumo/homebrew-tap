547d27eb58156291e25f3931b938553d15870d2b
makigumo <makigumo@users.noreply.github.com>
2025-06-11 00:16:40 +0200

    ryujinx@canary 1.3.84 (#3299)

--- Casks/ryujinx@canary.rb
+++ Casks/ryujinx@canary.rb
@@ -1,12 +1,20 @@
 cask "ryujinx@canary" do
-  version "1.3.58"
-  sha256 "f84e495298508bbb72b5e37b9f785fb35f54c2d9726dde03dbe3b0d917dd6f04"
+  version "1.3.84"
+  sha256 "61bd42d8c29c2eca9f79e107344611d75046be728dc565c3c605a30e6cba1efa"
 
-  url "https://github.com/GreemDev/Ryujinx-Canary/releases/download/#{version}/ryujinx-canary-#{version}-macos_universal.app.tar.gz",
-      verified: "github.com/GreemDev/Ryujinx-Canary/"
+  url "https://git.ryujinx.app/api/v4/projects/68/packages/generic/Ryubing-Canary/#{version}/ryujinx-canary-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
   desc "Experimental Nintendo Switch Emulator written in C#"
   homepage "https://ryujinx.app/"
+
+  livecheck do
+    url "https://git.ryujinx.app/api/v4/projects/68/releases"
+    strategy :json do |json, _regex|
+      json.map { |item| item["tag_name"] }
+    end
+  end
+
+  depends_on macos: ">= :monterey"
 
   app "Ryujinx.app", target: "Ryujinx Canary.app"
 
