ae9be17f5c5aed02025edc7cae12a9b13e14324d
Dan <dan@wadoku.de>
2025-08-11 12:39:53 +0200

    sevenzip 25.01

--- sevenzip.rb
+++ sevenzip.rb
@@ -1,26 +1,20 @@
 class Sevenzip < Formula
   desc "7-Zip is a file archiver with a high compression ratio"
   homepage "https://7-zip.org"
-  url "https://7-zip.org/a/7z2500-src.tar.xz"
-  version "25.00"
-  sha256 "bff9e69b6ca73a5b8715d7623870a39dc90ad6ce1f4d1070685843987af1af9b"
+  url "https://7-zip.org/a/7z2501-src.tar.xz"
+  version "25.01"
+  sha256 "ed087f83ee789c1ea5f39c464c55a5c9d4008deb0efe900814f2df262b82c36e"
   license all_of: ["LGPL-2.1-or-later", "BSD-3-Clause"]
+  head "https://github.com/ip7z/7zip.git", branch: "main"
 
   livecheck do
     url "https://7-zip.org/download.html"
-    regex(/>\s*Download\s+7-Zip\s+v?(\d+(?:\.\d+)+)[\s<]/im)
+    regex(/>\s*Download\s+7-Zip\s+v?(\d+(?:\.\d+)+)\s+\([^)]+?\)/im)
   end
 
   no_autobump! because: :incompatible_version_format
 
   def install
-    # See https://sourceforge.net/p/sevenzip/discussion/45797/thread/9c2d9061ce/#01e7
-    if OS.mac?
-      inreplace ["Common/FileStreams.cpp", "UI/Common/UpdateCallback.cpp"].map { |d| buildpath/"CPP/7zip"/d },
-                "sysmacros.h",
-                "types.h"
-    end
-
     cd "CPP/7zip/Bundles/Alone2" do
       mac_suffix = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch
       mk_suffix, directory = if OS.mac?
