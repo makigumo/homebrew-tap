08669e8e617402fe72e92f1092f486527cc21aa8
makigumo <makigumo@users.noreply.github.com>
2025-08-13 17:09:27 +0200

    warodai 20250813 (#3419)

--- Casks/warodai.rb
+++ Casks/warodai.rb
@@ -1,6 +1,6 @@
 cask "warodai" do
-  version "20250722"
-  sha256 "078e5de568b96da6d5360bc35c69fc1c196e2100d4105ca3aa7334f14d7ddbb8"
+  version "20250813"
+  sha256 "4ed9bd7dffae2b5dd4fa757dc6bd59e216e50a275efac03bbfb84e6607d887a4"
 
   url "https://github.com/makigumo/warodai-mac-dic/releases/download/#{version}/Warodai.dmg"
   name "Warodai for Mac"
