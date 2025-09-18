d5eca43cd9b30a3c080e0951d56ef9524082ebfd
makigumo <makigumo@users.noreply.github.com>
2025-09-18 09:54:09 +0200

    shadps4-qt 0.11.0 (#3476)

--- Casks/shadps4-qt.rb
+++ Casks/shadps4-qt.rb
@@ -1,12 +1,23 @@
 cask "shadps4-qt" do
-  version "0.10.0"
-  sha256 "844a1504bc8157343650af14b2fcc488a8ce2aacfcf0bbd664cd462fd3daba4b"
+  version "0.11.0"
+  sha256 "00b56c74b818f4597468c42d3ff0700d772ca095c0e7668a6e2ca6fe900af44d"
 
   url "https://github.com/shadps4-emu/shadPS4/releases/download/v.#{version}/shadps4-macos-qt-#{version}.zip",
       verified: "github.com/shadps4-emu/shadPS4/"
   name "shadps4-qt"
   desc "PS4 emulator"
   homepage "https://shadps4.net/"
+
+  livecheck do
+    url :url
+    regex(/(\d+\.\d+\.\d+)/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
+  end
 
   depends_on macos: ">= :sequoia"
 
