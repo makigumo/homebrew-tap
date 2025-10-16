f4f2abf2b013ae00e0029f79a4725812d537859c
Dan <dan@wadoku.de>
2025-10-16 10:52:59 +0200

    assetripper 1.3.5

--- Casks/assetripper.rb
+++ Casks/assetripper.rb
@@ -1,9 +1,9 @@
 cask "assetripper" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.3.4"
-  sha256 arm:   "ad1da5064aa048ad7458b6a48f96c4feec4bc1c68069801935b633e637331ee1",
-         intel: "4147fe8323d73cb3767bc06b3eda40c954596c3f18f887beb1b831f50ee39c90"
+  version "1.3.5"
+  sha256 arm:   "37f2fd0fc02682fcf4667d6b4b189577d3bf93da980029043f469c63058e1ac9",
+         intel: "145ee0e55bfd83b6f9e0796ec274821770f8078540e1b9f01920fca064d96186"
 
   url "https://github.com/AssetRipper/AssetRipper/releases/download/#{version}/AssetRipper_mac_#{arch}.zip"
   name "AssetRipper"
