cba1d9894e6db7cc4c728e95517e297c71cbaaf4
Dan <dan@wadoku.de>
2025-10-25 08:58:42 +0200

    jmdict 20251025.18798712011

--- Casks/jmdict.rb
+++ Casks/jmdict.rb
@@ -1,6 +1,6 @@
 cask "jmdict" do
-  version "20251018.18611302552"
-  sha256 "1ca471acb8718f08e63cfc593f49369ba8b6b71bed6b0bccc606a9511c300819"
+  version "20251025.18798712011"
+  sha256 "96ce3d303406954f71b17a35f346bdf3c6edd537cb4c7f9a1bc27e5909c54c37"
 
   url "https://github.com/makigumo/jmdict-mac-dic/releases/download/#{version}/JMDict.dmg"
   name "JMDict for Mac"
