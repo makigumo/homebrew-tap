47d40c739a2245b7e0f9d91324895a8c90f13e4a
Dan <dan@wadoku.de>
2025-10-31 01:50:56 +0100

    pcsx2@nightly 2.5.259

--- Casks/pcsx2@nightly.rb
+++ Casks/pcsx2@nightly.rb
@@ -1,12 +1,14 @@
 cask "pcsx2@nightly" do
-  version "2.5.258"
-  sha256 "853c103caedb298bde732f6702e228418617da6a9788b23267783d38702ead63"
+  version "2.5.259"
+  sha256 "4f53f2259832095ce8deec7d38cf821fc24226b195410cb5f95fb1a3a30965ab"
 
   url "https://github.com/PCSX2/pcsx2/releases/download/v#{version}/pcsx2-v#{version}-macos-Qt.tar.xz",
       verified: "github.com/PCSX2/pcsx2/"
   name "pcsx2-nightly"
   desc "Playstation 2 Emulator"
   homepage "https://pcsx2.net/"
+
+  depends_on macos: ">= :big_sur"
 
   app "PCSX2-v#{version}.app"
 
