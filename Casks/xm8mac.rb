244ecd46d7b3d495151a934980ae9374306c3b56
Dan <dan@wadoku.de>
2026-04-03 10:29:20 +0200

    xm8mac 1.7.8

--- Casks/xm8mac.rb
+++ Casks/xm8mac.rb
@@ -1,6 +1,6 @@
 cask "xm8mac" do
-  version "1.7.7"
-  sha256 "126f23c37f277a2af2b7032025b89ccf47ef92b260a75fa23eb00bdc37404ccc"
+  version "1.7.8"
+  sha256 "454e31dd7e8d1a882115c3117ce43b8d7c91c414e514a3dc68b351bcd8f536c4"
 
   url "https://github.com/bubio/xm8mac/releases/download/#{version}/XM8_macOS_Universal.dmg"
   name "xm8mac"
