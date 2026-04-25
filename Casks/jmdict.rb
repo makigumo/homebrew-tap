c48fbc5ec2f9c815a458fa997f57d2097bf0f1b0
makigumo <makigumo@users.noreply.github.com>
2026-04-25 11:09:27 +0200

    jmdict 20260425.24924528761 (#4011)

--- Casks/jmdict.rb
+++ Casks/jmdict.rb
@@ -1,6 +1,6 @@
 cask "jmdict" do
-  version "20260418.24598610819"
-  sha256 "7d45cc6966bded595565eea631fabc1d74fb8b52218037b63612adaad594f3e9"
+  version "20260425.24924528761"
+  sha256 "de31d114c319a649a23283ca0c5b34e286009a75db8abe91865312a60bd73e35"
 
   url "https://github.com/makigumo/jmdict-mac-dic/releases/download/#{version}/JMDict.dmg"
   name "JMDict for Mac"
