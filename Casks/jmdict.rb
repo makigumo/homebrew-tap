224209800f0631616944ca153f0f365d150dbdce
makigumo <makigumo@users.noreply.github.com>
2026-05-16 11:51:15 +0200

    jmdict 20260516.25955970514 (#4072)

--- Casks/jmdict.rb
+++ Casks/jmdict.rb
@@ -1,11 +1,13 @@
 cask "jmdict" do
-  version "20260509.25594984266"
-  sha256 "5b42f9aca5881c0e1437d4eec1c725f8d8529807a6c094f7e6abdbc8e0c16289"
+  version "20260516.25955970514"
+  sha256 "4b63ed5585d69ab6c0ff806d9f02a120d24a5e4d99877bd215e1c6367f68826a"
 
   url "https://github.com/makigumo/jmdict-mac-dic/releases/download/#{version}/JMDict.dmg"
   name "JMDict for Mac"
   desc "Japanese-English dictionary"
   homepage "https://github.com/makigumo/jmdict-mac-dic"
+
+  depends_on :macos
 
   dictionary "JMDict.dictionary"
 end
