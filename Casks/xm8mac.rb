129d07d6d8d8479694864d91cef06e2b881a9828
makigumo <makigumo@users.noreply.github.com>
2026-06-17 02:12:29 +0200

    xm8mac 1.7.9 (#4128)

--- Casks/xm8mac.rb
+++ Casks/xm8mac.rb
@@ -1,11 +1,13 @@
 cask "xm8mac" do
-  version "1.7.8"
-  sha256 "454e31dd7e8d1a882115c3117ce43b8d7c91c414e514a3dc68b351bcd8f536c4"
+  version "1.7.9"
+  sha256 "3eac2462046c8fcf29606b5733aafdddc7491041a736f28232d657da8a93c8b0"
 
   url "https://github.com/bubio/xm8mac/releases/download/#{version}/XM8_macOS_Universal.dmg"
   name "xm8mac"
   desc "PC-8801 emulator"
   homepage "https://github.com/bubio/xm8mac"
+
+  depends_on :macos
 
   app "XM8.app"
 end
